7647e7bf8238f0b3a92e95843c16394dbaf57b2e
Nulsarf <nomail@nomail.nomail>
2014-09-02 16:37:53 +0200

    Programme des boids fini et fonctionnel

--- boids/exo1.py
+++ boids/exo1.py
@@ -10,7 +10,7 @@ import math
 import random
 
 #Nombre de tortues
-n=20
+n=10
 #Instance de tortues
 boids=[]
 #vitesse
@@ -21,11 +21,14 @@ regle1=[]
 regle2=[0,0]
 regle3=[]
 regle4=[]
+regle5=[]
+v=[]
 #Coef des regles
-r1=.1
-r2=.1
-r3=.4
-r4=1.0
+r1=10
+r2=50
+r3=4
+r4=100
+r5=1
 
 turtle.tracer(50,1)
 turtle.color("white")
@@ -40,6 +43,8 @@ for i in range(n):
 	regle1.append([0,0])
 	regle3.append([0,0])
 	regle4.append([0,0])
+	regle5.append([0,0])
+	v.append([0,0])
 
 while True:
 	#Regle 1
@@ -84,23 +89,21 @@ while True:
 	for i in range(n):
 		regle4[i][0]=0
 		regle4[i][1]=0
-		if boids[i].xcor() > largeur/2:
-			regle4[i][0]=-10
-		elif boids[i].xcor() < -largeur/2:
-			regle4[i][0]=10
-		
-		if boids[i].ycor() > hauteur/2:
-			regle4[i][1]=-10
-		elif boids[i].ycor() < -hauteur/2:
-			regle4[i][1]=10
+		if boids[i].distance(0,0) > largeur:
+			regle4[i][0]=-boids[i].xcor()
+			regle4[i][1]=-boids[i].ycor()
 			
+	
+	#Regle 5
+	for i in range(n):
+		regle5[i][0]=v[i][0]
+		regle5[i][1]=v[i][1]
 	
 	#Application des regles
 	for i in range(n):
-		v=[0,0]
-		v[0] = r1 * regle1[i][0] + r2 * regle2[0] + r3 * regle3[i][0] + r4 * regle4[i][0]
-		v[1] = r1 * regle1[i][1] + r2 * regle2[1] + r3 * regle3[i][1] + r4 * regle4[i][1]
-		boids[i].setheading(math.atan2(v[1], v[0])*57.17)
+		v[i][0] = r1 * regle1[i][0] + r2 * regle2[0] + r3 * regle3[i][0] + r4 * regle4[i][0] + r5 * regle5[i][0]
+		v[i][1] = r1 * regle1[i][1] + r2 * regle2[1] + r3 * regle3[i][1] + r4 * regle4[i][1] + r5 * regle5[i][1]
+		boids[i].setheading(math.atan2(v[i][1], v[i][0])*57.17)
 		boids[i].forward(s)
 					
 raw_input("Appuyer sur ENTRER")
