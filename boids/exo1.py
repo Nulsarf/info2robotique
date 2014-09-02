a1e039df3e48ee3bc39f8fefb2ffee9e49c29a18
Nulsarf <nomail@nomail.nomail>
2014-09-02 16:03:55 +0200

    Regle 4 operationnel

--- boids/exo1.py
+++ boids/exo1.py
@@ -10,21 +10,25 @@ import math
 import random
 
 #Nombre de tortues
-n=10
+n=20
 #Instance de tortues
 boids=[]
 #vitesse
-s=8
+s=1
 #Zone de repulsion
 zdr=20
 regle1=[]
 regle2=[0,0]
 regle3=[]
+regle4=[]
 #Coef des regles
-r1=1.0
-r2=1.0
-r3=4.0
+r1=.1
+r2=.1
+r3=.4
+r4=1.0
 
+turtle.tracer(50,1)
+turtle.color("white")
 for i in range(n):
 	boids.append(turtle.Turtle())
 	boids[i].penup()
@@ -35,6 +39,7 @@ for i in range(n):
 	
 	regle1.append([0,0])
 	regle3.append([0,0])
+	regle4.append([0,0])
 
 while True:
 	#Regle 1
@@ -74,11 +79,27 @@ while True:
 			regle3[i][0] = boids[i].xcor() - pox
 			regle3[i][1] = boids[i].ycor() - poy
 	
+	#Regle 4	
+	hauteur, largeur = turtle.screensize()
+	for i in range(n):
+		regle4[i][0]=0
+		regle4[i][1]=0
+		if boids[i].xcor() > largeur/2:
+			regle4[i][0]=-10
+		elif boids[i].xcor() < -largeur/2:
+			regle4[i][0]=10
+		
+		if boids[i].ycor() > hauteur/2:
+			regle4[i][1]=-10
+		elif boids[i].ycor() < -hauteur/2:
+			regle4[i][1]=10
+			
+	
 	#Application des regles
 	for i in range(n):
 		v=[0,0]
-		v[0] = r1 * regle1[i][0] + r2 * regle2[0] + r3 * regle3[i][0]
-		v[1] = r1 * regle1[i][1] + r2 * regle2[1] + r3 * regle3[i][1]
+		v[0] = r1 * regle1[i][0] + r2 * regle2[0] + r3 * regle3[i][0] + r4 * regle4[i][0]
+		v[1] = r1 * regle1[i][1] + r2 * regle2[1] + r3 * regle3[i][1] + r4 * regle4[i][1]
 		boids[i].setheading(math.atan2(v[1], v[0])*57.17)
 		boids[i].forward(s)
 					
